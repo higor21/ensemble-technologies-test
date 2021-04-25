c081be74e7985b1c59f3ed20b7717877092dbe38
higor21 <felype.cet.15@gmail.com>
2021-04-25 12:16:31 -0300

    Setting API

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,18 +1,35 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 import { toast } from 'react-toastify';
+import store from 'store';
 
-const baseURL = '/api';
+const baseURL = 'https://job.ensemble.com.br/api/';
 
 const http = axios.create({
   baseURL,
 });
 
+http.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    const { authToken } = store.getState().auth
+
+    config.headers['ens-api-token'] = 'y50JmeTrhLdG0HDrAfAk';
+
+    if (authToken) {
+      config.headers['ens-auth-token'] = authToken;
+    }
+
+    return config;
+  }
+)
+
 http.interceptors.response.use(
   ({ data }: AxiosResponse) => data,
-  (error) => {
+  (error: AxiosError) => {
+    console.log(error)
+    /* if (error.)
     if (error.response.status / 500 === 1) {
       toast.error('Server temporarily in error.\nPlease try again later.\t:(')
-    }
+    } */
     return Promise.reject(error);
   }
 );
